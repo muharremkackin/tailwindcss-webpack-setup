c93dc1dadde46b51504eb588c08d919beb0ed61d
Muharrem Kaçkın <muharremkackin@gmail.com>
2020-11-08 20:57:58 +0300

    updated for my projects

--- app.js
+++
@@ -1,6 +0,0 @@
-/**
- * Import our style css sheet here.
- * We use a webpack loader called style-loader to inject our
- * css into the DOM
- */
-import "./style.css";
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
-/**
- * We add all .html files to the purge array here.
- * When NODE_ENV is set to 'production', tailwind will
- * automatically purge these files and removed unused css
- */
-
 module.exports = {
-  purge: ["./**/*.html"],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-};
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: ["./**/*.html"],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+}
